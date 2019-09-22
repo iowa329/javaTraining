02ebf9059c19becefc588f887426a10584f4f901
iowa329 <iowa329@gmail.com>
2019-09-22 23:23:29 +0900

    01_save

---
+++ src/javaTraining/Baekjoon_2839.java
@@ -0,0 +1,32 @@
+package javaTraining;
+import java.util.Scanner;
+
+public class Baekjoon_2839 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		int five = num/5;
+		int three = -1;
+		int rest = 0;
+		
+		rest = num%5;
+		
+		while(five>=0) {
+			if(rest%3 == 0) {
+				three = rest/3;
+			} else {
+				five--;
+				if(five>0)
+					rest = num/five;
+			}
+		}
+		
+		if(three == -1) {
+			System.out.println(three);
+		} else {
+			System.out.println(five+three);
+		}
+		
+	}
+}
