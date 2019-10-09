0496ce1f21be86015483d15d30c68da39ea6ad61
iowa329 <iowa329@gmail.com>
2019-10-09 20:03:24 +0900

    03_LinkedStack으로 구현 성공 / 저장된 수 합 구하는 함수구현 필요

--- src/javaTraining/Baejoon_10773.java
+++ src/javaTraining/Baejoon_10773.java
@@ -1,6 +1,65 @@
 package javaTraining;
 import java.util.Scanner;
 
+class Node {
+	int num;
+	Node next;
+}
+
+class LinkedStack {
+	Node top;
+	
+	public LinkedStack(){
+		top = null;
+	}
+	
+	public void push(int num){
+		if(num == 0) {
+			top = top.next;
+		} else {
+			Node node = new Node();
+			node.num = num;
+			node.next = top;
+			top = node;
+//			System.out.print(top.num);
+		}
+	}
+	
+	public void popAll() {
+		while(top != null) {
+			System.out.print(top.num);
+			top = top.next;
+		}
+		System.out.println("\nDone.");
+//		int num = top.num;
+//		return top.num;
+//		top = top.next;
+//		return num;
+	}
+}
+
+public class Baejoon_10773 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		System.out.println();
+		
+		LinkedStack stack = new LinkedStack();
+		
+		for(int i=1; i<=count; i++)
+			stack.push(sc.nextInt());
+
+		
+		stack.popAll();
+//		System.out.println();
+		
+		sc.close();
+	}
+}
+
+
+/*
 //백준 문제 10773번 풀이_김희재
 public class Baejoon_10773 {
 	public static void main(String[] args) {
@@ -9,8 +68,6 @@ public class Baejoon_10773 {
 		
 		//배열 초기화
 		int num[] = new int[count];
-		for(int i=0; i<num.length; i++)
-			num[i]=0;
 		
 		//입력값을 배열에 저장
 		int pos = 0; //저장되고 있는 배열의 위치 인덱스값
@@ -32,3 +89,4 @@ public class Baejoon_10773 {
 		System.out.println(sum);
 	}
 }
+*/
