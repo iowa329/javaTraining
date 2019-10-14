3eacd072e1784c495de0f469cb4b54a3fe32b892
iowa329 <iowa329@gmail.com>
2019-10-14 20:24:57 +0900

    05_구현완성

--- src/javaTraining/Baekjoon_8958.java
+++ src/javaTraining/Baekjoon_8958.java
@@ -1,6 +1,7 @@
 package javaTraining;
 import java.util.Scanner;
 
+//백준 문제 8958번 풀이_김희재
 public class Baekjoon_8958 {
 	
 	public static void main(String[] args) {
@@ -11,17 +12,16 @@ public class Baekjoon_8958 {
 		for(int i=1; i<=line; i++) {
 			String quiz = sc.nextLine();
 			int sum = 0, value = 0;
-			for(int j=0; j<quiz.length(); j++){
-				
+			for(int j=0; j<quiz.length(); j++){	
 				if(quiz.charAt(j) == 'O') {
 					value++;
 					sum += value;
-				} else {
+				} else
 					value = 0;
-				}
 			}
 			System.out.println(sum);
-		}	
+		}
+		sc.close();
 	}
 
 }
