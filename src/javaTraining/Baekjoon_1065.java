b351e2d9a109c31f8583d989c37d454830c3ed96
iowa329 <iowa329@gmail.com>
2019-10-22 00:29:54 +0900

    03_구현 아이디어 수정. 해당 숫자의 자리수 마다 변수로 지정

--- src/javaTraining/Baekjoon_1065.java
+++ src/javaTraining/Baekjoon_1065.java
@@ -8,19 +8,32 @@ public class Baekjoon_1065 {
 		int num = sc.nextInt();
 		sc.close();
 		int count = 0;
-		
-		int check;
-		if(num<10) {
-			check = 0;
-		} else if(num>=10 && num<100) {
-			check = 10;
-		} else if(num>=100 && num<1000) {
-			check = 100;
-		} else {
-			check = 1000;
-		}
-		
+
+		int diff1=0; diff2=0;
+		int one, ten, hun, thou;
 		for(int i=1;i<=num;i++) {
+			one = i%10;
+			ten = i%100 - one;
+			hun = i - ten - one;
+			thou = i;
+			if(num<10) {
+				count++;
+			} else if(num>=10 && num<100) {
+				count++;
+			} else if(num>=100 && num<1000) {
+				check = 100;
+			} else {
+				check = 1000;
+			}
+			
+			for(int j=10; j<=1000; j=j^10) {
+				if(i%j != 0) {
+					diff1 = i%j - i%(j^10);
+					diff2 = i%(j^10) - i%(j^100);
+				}
+				if(diff1 == diff2)
+					count++;
+			}
 			
 		}
 		System.out.println(count);
