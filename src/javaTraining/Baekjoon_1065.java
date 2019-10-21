3bb1a06ad443a498b1ffbcfe50b802035c7b5c05
iowa329 <iowa329@gmail.com>
2019-10-22 00:34:09 +0900

    04_1차 구현 완성. 결과값 오류발생(0으로 출력)

--- src/javaTraining/Baekjoon_1065.java
+++ src/javaTraining/Baekjoon_1065.java
@@ -9,32 +9,23 @@ public class Baekjoon_1065 {
 		sc.close();
 		int count = 0;
 
-		int diff1=0; diff2=0;
-		int one, ten, hun, thou;
+		int diff1=0, diff2=0;
+		int one, ten, hun;
 		for(int i=1;i<=num;i++) {
 			one = i%10;
 			ten = i%100 - one;
 			hun = i - ten - one;
-			thou = i;
 			if(num<10) {
 				count++;
 			} else if(num>=10 && num<100) {
 				count++;
 			} else if(num>=100 && num<1000) {
-				check = 100;
-			} else {
-				check = 1000;
-			}
-			
-			for(int j=10; j<=1000; j=j^10) {
-				if(i%j != 0) {
-					diff1 = i%j - i%(j^10);
-					diff2 = i%(j^10) - i%(j^100);
-				}
+				diff1 = one - ten;
+				diff2 = ten - hun;
 				if(diff1 == diff2)
 					count++;
+			} else {
 			}
-			
 		}
 		System.out.println(count);
 	}
