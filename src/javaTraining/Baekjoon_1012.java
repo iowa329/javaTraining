b67efcbe1f26aad99585bdaa75993561ae02a809
iowa329 <iowa329@gmail.com>
2019-10-28 17:00:42 +0900

    03_IndexOutOfBoundsException 오류에 의한 수정(배열 겉에 껍데기 배열값 생성)

--- src/javaTraining/Baekjoon_1012.java
+++ src/javaTraining/Baekjoon_1012.java
@@ -3,35 +3,38 @@ import java.util.Scanner;
 
 public class Baekjoon_1012 {
 	
-	public static void main(String[] args[]) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int cnt = sc.nextInt();
-		
-		int width = sc.nextInt();
-		int height = sc.nextInt();
-		int cabbage = sc.nextInt();
-		int[][] field = new int[width][height];
-		
-		for(int i=1; i<=cabbage; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			field[x][y] = 1;
-		}
-		
-		int earthworm = 0;
-		for(int i=0; i<field.length; i++) {
-			for(int j=0; j<field[i].length; j++) {
-				if(field[i][j] == 1) {
-					if(field[i-1][j] == 2 || field[i+1][j] == 2 || field[i][j-1] == 2 || field[i][j+1] == 2) {
-						field[i][j] = 2;
-					} else {
-						earthworm++;
-						field[i][j] = 2;
-					}
-				}//배추가 심어져 있는 경우 처리
+		for(int num=1; num<=cnt; num++){
+			int width = sc.nextInt();
+			int height = sc.nextInt();
+			int cabbage = sc.nextInt();
+			int[][] field = new int[width+1][height+1];
+			
+			for(int i=1; i<=cabbage; i++) {
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+				field[x+1][y+1] = 1;
+			}
+			
+			int earthworm = 0;
+			for(int i=1; i<field.length-1; i++) {
+				for(int j=1; j<field[i].length-1; j++) {
+					if(field[i][j] == 1) {
+						if(field[i-1][j] == 2 || field[i+1][j] == 2 || field[i][j-1] == 2 || field[i][j+1] == 2) {
+							field[i][j] = 2;
+						} else {
+							earthworm++;
+							field[i][j] = 2;
+						}
+					}//배추가 심어져 있는 경우 처리
+				}
 			}//배추밭 탐색 종료
-		}//주어진 밭의 경우의 수 종료
-		
-		
+			
+			System.out.println(earthworm);
+		}
+		sc.close();
 	}
+	
 }
