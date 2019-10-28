83f0b2a2a815c013efabede05be2e1ecbf677df0
iowa329 <iowa329@gmail.com>
2019-10-28 14:39:25 +0900

    02_배추밭 탐색 알고리즘 구현02_배추밭 탐색 알고리즘 구현

--- src/javaTraining/Baekjoon_1012.java
+++ src/javaTraining/Baekjoon_1012.java
@@ -18,7 +18,20 @@ public class Baekjoon_1012 {
 			field[x][y] = 1;
 		}
 		
+		int earthworm = 0;
+		for(int i=0; i<field.length; i++) {
+			for(int j=0; j<field[i].length; j++) {
+				if(field[i][j] == 1) {
+					if(field[i-1][j] == 2 || field[i+1][j] == 2 || field[i][j-1] == 2 || field[i][j+1] == 2) {
+						field[i][j] = 2;
+					} else {
+						earthworm++;
+						field[i][j] = 2;
+					}
+				}//배추가 심어져 있는 경우 처리
+			}//배추밭 탐색 종료
+		}//주어진 밭의 경우의 수 종료
+		
 		
 	}
-
 }
