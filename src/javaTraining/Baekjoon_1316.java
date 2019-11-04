ce8b0036c31a2b373d4595ed109b213b41ce4b09
iowa329 <iowa329@gmail.com>
2019-11-04 22:52:40 +0900

    03_main함수 매개변수 작성누락 오류

--- src/javaTraining/Baekjoon_1316.java
+++ src/javaTraining/Baekjoon_1316.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class Baekjoon_1316 {
 	
-	public static void main() {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int cnt = sc.nextInt();
+		sc.nextLine();
 		
 		int grpwd = cnt;
 		
