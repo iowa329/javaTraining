28202f12b56c56baab31809017d61536bc36b72c
iowa329 <iowa329@gmail.com>
2019-11-11 00:55:02 +0900

    02_거리 비교 방정식으로 수정

--- src/javaTraining/Baekjoon_1002.java
+++ src/javaTraining/Baekjoon_1002.java
@@ -5,43 +5,37 @@ public class Baekjoon_1002 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int cnt = sc.nextInt();
-		sc.next();
+		//sc.next();
 		
 		for(int i=1; i<=cnt; i++) {
 			int x1 = sc.nextInt();
+			System.out.print(x1+" ");
 			int y1 = sc.nextInt();
+			System.out.print(y1+" ");
 			int r1 = sc.nextInt();
+			System.out.print(r1+" ");
 			int x2 = sc.nextInt();
+			System.out.print(x2+" ");
 			int y2 = sc.nextInt();
+			System.out.print(y2+" ");
 			int r2 = sc.nextInt();
-			int x3=0, y3=0;
+			System.out.println(r2);
 			
-			int case1=0, case2=0;
-			int result=0;
+			int cases = 0;
+			int radiusSum = (int) Math.pow((r1+r2), 2);
+			System.out.print(radiusSum+" ");
+			int result = (int) Math.pow((x1-x2)+(y1-y2), 2);
+			System.out.println(result+" ");
 			
-			result = r1^2 - (x1-x3)^2;
-			switch (result) {
-				case 0 :
-					case1 = 1;
-				default :
-					if(result > 0)
-						case1 = 2;
-					else
-						case1 = 0;
+			if(result == radiusSum) {
+				cases = 1;
+			} else if(result < radiusSum) {
+				cases = 2;
+			} else if(result > radiusSum) {
+				cases = -1;
 			}
 			
-			result = r2^2 - (x2-x3)^2;
-			switch (result) {
-				case 0 :
-					case2 = 1;
-				default :
-					if(result > 0)
-						case2 = 2;
-					else
-						case2 = 0;
-			}
-			
-			System.out.println(case1+case2);
+			System.out.println(cases);
 		}
 		
 		
