91021a68fa03f555a4359ae8dac2bd9ca1231e1a
iowa329 <iowa329@gmail.com>
2019-11-11 01:46:52 +0900

    03_반례수정

--- src/javaTraining/Baekjoon_1002.java
+++ src/javaTraining/Baekjoon_1002.java
@@ -9,35 +9,36 @@ public class Baekjoon_1002 {
 		
 		for(int i=1; i<=cnt; i++) {
 			int x1 = sc.nextInt();
-			System.out.print(x1+" ");
 			int y1 = sc.nextInt();
-			System.out.print(y1+" ");
 			int r1 = sc.nextInt();
-			System.out.print(r1+" ");
 			int x2 = sc.nextInt();
-			System.out.print(x2+" ");
 			int y2 = sc.nextInt();
-			System.out.print(y2+" ");
 			int r2 = sc.nextInt();
-			System.out.println(r2);
 			
 			int cases = 0;
+			int distance = (int) Math.pow((x1-x2)+(y1-y2), 2);
 			int radiusSum = (int) Math.pow((r1+r2), 2);
-			System.out.print(radiusSum+" ");
-			int result = (int) Math.pow((x1-x2)+(y1-y2), 2);
-			System.out.println(result+" ");
+			int radiusDiff = (int) Math.pow((r1-r2), 2);
 			
-			if(result == radiusSum) {
-				cases = 1;
-			} else if(result < radiusSum) {
-				cases = 2;
-			} else if(result > radiusSum) {
-				cases = -1;
+			if(distance == 0) {
+				if(r1 == r2)
+					cases = -1;
+				else
+					cases = 0;
+			} else {
+				if(distance == radiusSum || distance == radiusDiff){
+					cases = 1;
+				} else if(distance < radiusSum)
+					cases = 2;
+				else {
+					cases = 0;
+				}
+				
 			}
 			
 			System.out.println(cases);
 		}
-		
+		sc.close();
 		
 		
 	}
