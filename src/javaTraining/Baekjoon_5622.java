eab89bab3654420c587e429cfc4e12adc860805c
iowa329 <iowa329@gmail.com>
2019-11-18 18:02:44 +0900

    02_구현완성

--- src/javaTraining/Baekjoon_5622.java
+++ src/javaTraining/Baekjoon_5622.java
@@ -7,56 +7,51 @@ public class Baekjoon_5622 {
 		Scanner sc = new Scanner(System.in);
 		String dial = sc.next();
 		sc.close();
-		
+
 		int time=0;
 		for(int i=0; i<dial.length(); i++) {
 			switch(dial.charAt(i)) {
 			case 'A':
 			case 'B':
 			case 'C':
-				time =+ 2;
+				time += 3;
 				break;
 			case 'D':
 			case 'E':
 			case 'F':
-				time =+ 3;
+				time += 4;
 				break;
 			case 'G':
 			case 'H':
 			case 'I':
-				time =+ 4;
+				time += 5;
 				break;
 			case 'J':
 			case 'K':
 			case 'L':
-				time =+ 5;
+				time += 6;
 				break;
 			case 'M':
 			case 'N':
 			case 'O':
-				time =+ 6;
+				time += 7;
 				break;
 			case 'P':
 			case 'Q':
 			case 'R':
 			case 'S':
-				time =+ 7;
+				time += 8;
 				break;
 			case 'T':
 			case 'U':
 			case 'V':
-				time =+ 8;
+				time += 9;
 				break;
 			case 'W':
 			case 'X':
 			case 'Y':
 			case 'Z':
-				time =+ 9;
-				break;
-			case 'a':
-			case 'b':
-			case 'c':
-				time =+ 2;
+				time += 10;
 				break;
 			}
 		}
