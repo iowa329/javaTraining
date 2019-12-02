90a8a05981b90a51dc4632c585069680bbb4a3b1
iowa329 <iowa329@gmail.com>
2019-12-02 21:53:31 +0900

    04_main의 static 입력실수 수정

--- src/javaTraining/ReduceString.java
+++ src/javaTraining/ReduceString.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class ReduceString {
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
 		sc.close();
@@ -30,6 +30,7 @@ public class ReduceString {
 				shortest = result.length();
 			cases++;
 		}
+		System.out.println(result);
 		System.out.println(shortest);
 	}
 
