d8de9216794d28c2b2f78b4e58e63869ffcf37cb
iowa329 <iowa329@gmail.com>
2019-12-02 21:49:03 +0900

    03_결과값 프린트문 추가

--- src/javaTraining/ReduceString.java
+++ src/javaTraining/ReduceString.java
@@ -10,24 +10,27 @@ public class ReduceString {
 		
 		int cases = 1;
 		String result = null;
-		int shortest = 0;
+		int shortest = s.length();
+		int cnt = 1;
 		while(cases <= s.length()) {
 			for(int pos=0; pos+cases <= s.length(); pos+=cases) {
 				String wd1 = s.substring(pos, pos+cases-1);
 				pos += cases;
 				String wd2 = s.substring(pos, pos+cases-1);
 				
-				int cnt=1;
 				if(wd1 == wd2) {
 					cnt++;
 					result = result + (cnt+wd1);
+				} else {
+					result = result + wd1;
+					cnt = 1;
 				}
 			}
 			if(result.length() < shortest)
 				shortest = result.length();
 			cases++;
 		}
-		
+		System.out.println(shortest);
 	}
 
 }
