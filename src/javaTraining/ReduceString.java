5f0bf70c42e2db89af4f91786aaf5b52222db810
iowa329 <iowa329@gmail.com>
2019-12-03 21:50:33 +0900

    14_for문 while문으로 수정. 구현완료

--- src/javaTraining/ReduceString.java
+++ src/javaTraining/ReduceString.java
@@ -1,21 +1,21 @@
 package javaTraining;
 import java.util.Scanner;
 
+//구현문제_02_어피치_문자열_압축하기
 public class ReduceString {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
 		sc.close();
-		
-		int cases = 1;
+
 		int shortest = s.length();
 		int cnt = 1;
-		String result=""; //프린트문을 위한 임시 변수 선언(원래는 안에다가)
+		int cases = 1;
 		while(cases <= s.length()) {
-			/*String*/result = cnt + s.substring(0,cases);
+			String result = cnt + s.substring(0,cases);
 			int pos=0;
-			for(; pos+(cases*2) <= s.length();) {
+			while(pos+(cases*2) <= s.length()) {
 				String wd1 = s.substring(pos, pos+cases);
 				pos += cases;
 				String wd2 = s.substring(pos, pos+cases);
@@ -25,31 +25,18 @@ public class ReduceString {
 				} else {
 					cnt = 1;
 					result = result + (cnt+wd2);
-					
 				}
 			}
-			cnt = 1;
-			
 			if(s.length() % cases != 0)
 				result = result + s.substring(pos+cases,s.length());
-			
 			result = result.replace("1","");
+			
 			if(result.length() < shortest)
 				shortest = result.length();
-
-			System.out.println(result + " (" + cases + ")=>" + shortest);
 			
-			/*if(cases < s.length()) {
-				do {
-					cases++;
-					
-				} while(s.length() % cases != 0);
-			} else {
-				cases++;
-			}*/
+			cnt = 1;
 			cases++;
 		}
-		System.out.println(result);
 		System.out.println(shortest);
 	}
 
