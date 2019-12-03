4aaf692a63d89a4e6dc201e3b92977fd732f697b
iowa329 <iowa329@gmail.com>
2019-12-03 20:19:10 +0900

    10_if문 조건 안 String 객체 비교 연산자 사용 오류 수정

--- src/javaTraining/ReduceString.java
+++ src/javaTraining/ReduceString.java
@@ -11,15 +11,15 @@ public class ReduceString {
 		int cases = 1;
 		int shortest = s.length();
 		int cnt = 1;
-		String result = s.substring(0,1);
+		String result = cnt + s.substring(0,1);
 		while(cases <= s.length()) {
 			for(int pos=0; pos+(cases*2) <= s.length();) {
 				String wd1 = s.substring(pos, pos+cases);
 				pos += cases;
 				String wd2 = s.substring(pos, pos+cases);
 				
-				if(wd1 == wd2) {
-					result = result + s.replace(cnt+wd1, ++cnt+wd1);
+				if(wd1.equals(wd2)) {
+					result = result.replace(cnt+wd2, ++cnt+wd2);
 				} else {
 					cnt = 1;
 					result = result + (cnt+wd2);
