d1af2da4eac0144e594bf2977a887da1c21e0067
iowa329 <iowa329@gmail.com>
2019-12-09 21:30:30 +0900

    구현 2차 작성

--- src/javaTraining/RangeBracket.java
+++ src/javaTraining/RangeBracket.java
@@ -3,15 +3,10 @@ import java.util.Scanner;
 
 public class RangeBracket {
 	
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		String w = sc.next();
-		sc.close();
+	static String range(String w) {
+		if(w.equals("")) return w;
 		
-		if(w.equals(""))
-			return "";
-		
-		String u,v;
+		String u="",v="";
 		int r=0, l=0;
 		for(int i=0; i<w.length(); i++) {
 			if(w.charAt(i) == '(') {
@@ -27,17 +22,36 @@ public class RangeBracket {
 				i = w.length();
 			}
 		}
-
+		
 		int open = 0;
-		for(int i=0; open < 0 || i < u.length(); i++) {
+		for(int i=0; open < 0 || i >= u.length(); i++){
 			if(u.charAt(i) == '(') {
 				open++;
 			} else {
 				open--;
 			}
 		}
-		if(open < 0)
-			
+		if(open >= 0) {
+			return range(v);
+		} else {
+			String s = "(";
+			s += v;
+			s += ")";
+			u = u.substring(1,u.length()-1);
+			u = u.replace("(",")");
+			u = u.replace(")","(");
+			s += u;
+			return s;
+		}
+	}
+	
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String w = sc.next();
+		sc.close();
+		
+		range(w);
 	}
 	
 }
